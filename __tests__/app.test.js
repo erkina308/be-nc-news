7d1e92607f539503f1e414057771e27de134bb43
erkina308 <erkinayna@hotmail.co.uk>
2024-01-16 13:03:23 +0000

    this commit contains the /api endpoint with new controller and model and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed.js");
 const testData = require("../db/data/test-data/index.js");
+const endpoints = require("../endpoints.json");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
@@ -18,6 +19,16 @@ describe("/api/topics", () => {
           expect(typeof item.slug).toBe("string");
           expect(typeof item.description).toBe("string");
         });
+      });
+  });
+});
+
+describe("/api", () => {
+  test("GET: 200 responds with an object describing all available endpoints on API", () => {
+    return request(app)
+      .get("/api")
+      .then(() => {
+        expect(endpoints).toEqual(endpoints);
       });
   });
 });
