8488eff0192b12ccda2e0f9ecea07de68bb6858e
erkina308 <erkinayna@hotmail.co.uk>
2024-02-04 11:24:38 +0000

    tasks

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -9,7 +9,7 @@ module.exports = [
     username: 'grumpy19',
     name: 'Paul Grump',
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013'
+      'https://vignette.wikia.nocookie.net/mrmen/images'
   },
   {
     username: 'happyamy2016',
